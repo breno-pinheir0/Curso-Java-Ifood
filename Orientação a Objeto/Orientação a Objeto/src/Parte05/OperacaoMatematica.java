68d94eea22842515e0430a2bcf616cff34f961d5
breno_pinheir0 <breno.oliveira@hotmail.com.br>
2023-05-08 16:38:44 -0300

    .

---
+++ Orientação a Objeto/Orientação a Objeto/src/Parte05/OperacaoMatematica.java
@@ -0,0 +1,12 @@
+package Parte05;
+
+interface OperacaoMatematica {
+    void soma(double num1, double num2);
+
+    void subtracao(double num1, double num2);
+
+    void multiplicacao(double num1, double num2);
+
+    void divisao(double num1, double num2);
+}
+

87819eaf4f2559e0d0bfb7bc839c7479eb22a229
breno_pinheir0 <breno.oliveira@hotmail.com.br>
2023-05-11 17:54:53 -0300

    Terminando o 2 exercício proposto

--- Collections/src/PerguntasCrime.java
+++ Collections/src/PerguntasCrime.java
@@ -15,31 +15,51 @@ import java.util.*;
 public class PerguntasCrime {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        List <String>Respostas = new ArrayList<String>();
-        String resp = "";
+        List <Character>Respostas = new ArrayList<Character>();
+        char resp;
         int contador = 0;
 
         System.out.println("Você telefonou para a vítima? (S para sim e N para não): ");
-        resp = scan.nextLine();
+        resp = scan.nextLine().charAt(0);
         Respostas.add(0, resp);
 
         System.out.println("Você esteve no local do crime? (S para sim e N para não): ");
-        resp = scan.nextLine();
+        resp = scan.nextLine().charAt(0);
         Respostas.add(1, resp);
 
         System.out.println("Você mora perto da vítima? (S para sim e N para não): ");
-        resp = scan.nextLine();
+        resp = scan.nextLine().charAt(0);
         Respostas.add(2, resp);
 
         System.out.println("Você devia para a vítima? (S para sim e N para não): ");
-        resp = scan.nextLine();
+        resp = scan.nextLine().charAt(0);
         Respostas.add(3, resp);
 
         System.out.println("Você já trabalhou com a vítima? (S para sim e N para não): ");
-        resp = scan.nextLine();
+        resp = scan.nextLine().charAt(0);
         Respostas.add(4, resp);
 
-    
+        for(int i = 0; i < Respostas.size(); i++){
+            if(Respostas.get(i) == 'S'){
+                contador++;
+            }
+        }
+
+        if(contador < 2){
+            System.out.println("Inocente.");
+        }
+
+        if(contador == 2){
+            System.out.println("Suspeita.");
+        }
+
+        if(contador == 3 || contador == 4){
+            System.out.println("Cúmplice.");
+        }
+
+        if(contador == 5){
+            System.out.println("Assassina.");
+        }
     
     
     

2c100329e23a19bd6e8b0201e4b6d989d75a1421
breno-pinheir0 <breno.oliveira@hotmail.com.br>
2023-05-12 17:48:55 -0300

    .

---
+++ Collections/src/ExemploSet.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ExemploSet {
+    public static void main(String[] args) {
+        System.out.println("Crie um conjunto e adicione as notas: ");
+        Set<Double> notas = new HashSet<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6)); // não mantém a ordem de inserção e não adiciona valores duplicados.
+        System.out.println(notas.toString());
+    }   
+}

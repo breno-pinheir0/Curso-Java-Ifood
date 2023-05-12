34290c27f0f66fbe24534aae1ae14115fbf3619b
breno-pinheir0 <breno.oliveira@hotmail.com.br>
2023-05-12 19:14:29 -0300

    .

--- Collections/src/ExemploOrdenacaoSet.java
+++ Collections/src/ExemploOrdenacaoSet.java
@@ -4,7 +4,12 @@ public class ExemploOrdenacaoSet {
     public static void main(String[] args) {
         
         System.out.println("--\tOrdem aleatória\t--");
-
+        Set<Serie> minhasSeries = new HashSet<>(){{
+            add(new Serie("got", "fantasia", 60));
+            add(new Serie("dark", "drama", 60));
+            add(new Serie("that '70s show", "comédia", 25));
+        }};
+        for(Serie serie : minhasSeries) System.out.println(serie.getNome() + " - " + serie.getGenero() + " - " + serie.getTempoEpisodio());
 
     }
 }

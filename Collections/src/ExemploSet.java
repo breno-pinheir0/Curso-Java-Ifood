b11425e419a1333f44eb1815cc02b55049572ace
breno-pinheir0 <breno.oliveira@hotmail.com.br>
2023-05-12 17:54:39 -0300

    .

--- Collections/src/ExemploSet.java
+++ Collections/src/ExemploSet.java
@@ -4,6 +4,12 @@ public class ExemploSet {
     public static void main(String[] args) {
         System.out.println("Crie um conjunto e adicione as notas: ");
         Set<Double> notas = new HashSet<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6)); // não mantém a ordem de inserção e não adiciona valores duplicados.
-        System.out.println(notas.toString());
+        System.out.println(notas.toString()); // Transformando de Double para String
+
+        System.out.println("Confira se a nota 5.0 está no conjunto: " + notas.contains(5d));
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
     }   
 }

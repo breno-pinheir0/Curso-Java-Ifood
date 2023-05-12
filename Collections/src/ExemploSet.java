e00078b5bf65fe7ea9d117ad4502d5f816dcdfdc
breno-pinheir0 <breno.oliveira@hotmail.com.br>
2023-05-12 18:11:28 -0300

    .

--- Collections/src/ExemploSet.java
+++ Collections/src/ExemploSet.java
@@ -11,5 +11,49 @@ public class ExemploSet {
         System.out.println("Exiba a menor nota: " + Collections.min(notas));
 
         System.out.println("Exiba a maior nota: " + Collections.max(notas));
-    }   
+
+        
+        Iterator<Double> iterator = notas.iterator(); // usado para acessar elementos de uma coleção 
+        Double soma = 0.0;
+        while(iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        }
+
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma/ notas.size()));
+
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        iterator = notas.iterator();
+        while(iterator.hasNext()){
+            Double next = iterator.next();
+            if(next < 7) iterator.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Exiba as notas na ordem que foram informadas: ");
+        Set<Double> notas2 = new LinkedHashSet<>(); //LinkedHashSet mantém a ordem dos elementos de acordo com a inserção
+        notas.add(7d);
+        notas2.add(8.5);
+        notas2.add(9.3);
+        notas2.add(5d);
+        notas2.add(7d);
+        notas2.add(0d);
+        notas2.add(3.6);
+        System.out.println(notas2);
+
+        System.out.println("Exiba todas as notas na ordem crescente: ");
+        Set<Double> notas3 = new TreeSet<>(notas2); // TreeSet ordena de acordo com a ordem natural dos elementos. Só funciona porque a classe Double possui comparator
+        System.out.println(notas3);
+
+        System.out.println("Apague todo o conjunto");
+        notas.clear();
+
+        System.out.println("Confira se o conjunto está vazio: " + notas.isEmpty());
+    }
 }

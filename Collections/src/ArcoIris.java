f1a459dda4ded9d2d07b4c0533db1f8db2e482b8
breno_pinheir0 <breno.oliveira@hotmail.com.br>
2023-05-13 22:59:11 -0300

    .

---
+++ Collections/src/ArcoIris.java
@@ -0,0 +1,25 @@
+import java.util.*;
+/*
+ * Crie um conjunto contendo as cores do arco-íris e:
+ * A. Exiba todas as cores uma abaixo da outra
+ * B. Exiba a quantidade de cores que o arco-íris tem
+ * C. Exiba as cores em ordem alfabética
+ * D. Exiba as cores na ordem inversa da que foi informada
+ * E. Exiba todas as cores que começam com a letra "v"
+ * F. Remova todas as cores que não começam com a letra "v"
+ * G. Limpe o conjunto
+ * H. Confira se o conjunto está vazio
+ */
+public class ArcoIris {
+    public static void main(String[] args) {
+        Set<String> cores = new LinkedHashSet<>();
+        cores.add("Azul");
+        cores.add("Vermelho");
+        cores.add("Laranja");
+        cores.add("Amarelo");
+        cores.add("Verde");
+        cores.add("Violeta");
+    }
+    
+
+}

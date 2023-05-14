f2ffdc4c8ae9c18debcc0ac2a76949db364df082
breno_pinheir0 <breno.oliveira@hotmail.com.br>
2023-05-14 10:07:46 -0300

    .

---
+++ Collections/src/Exercicio04.java
@@ -0,0 +1,21 @@
+public class Exercicio04 {
+    
+}
+
+class LinguagemFavorita{
+    private String nome;
+    private int anoDeCriacao;
+    private String ide;
+
+    String getNome(){
+        return nome;
+    }
+
+    int getAnoDeCriacao(){
+        return anoDeCriacao;
+    }
+
+    String getIde(){
+        return ide;
+    }
+}

62a884718699ba0514a5f847cf720935cc260b21
Breno Pinheiro de Oliveira <62961937+breno-pinheir0@users.noreply.github.com>
2023-10-24 13:47:59 -0300

    voltando aos estudos depois de um tempo

--- Exceções/ExceptionCustomizada_1.java
+++ Exceções/ExceptionCustomizada_1.java
@@ -1,3 +1,61 @@
+import javax.swing.*;
+import java.io.*;
+
 public class ExceptionCustomizada_1 {
-    
+    public static void main(String[] args) {
+        String nomeDoArquivo = JOptionPane.showInputDialog("Nome do arquivo a ser exibido: ");
+
+        imprimirArquivoNoConsole(nomeDoArquivo);
+        System.out.println("\nCom exception ou não, o programa continua...");
+    }
+
+    public static void imprimirArquivoNoConsole(String nomeDoArquivo) {
+        try{
+            BufferedReader br = lerArquivo(nomeDoArquivo);
+            String line = br.readLine();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            do{
+                bw.write(line);
+                bw.newLine();
+                line = br.readLine();
+            } while(line != null);
+            bw.flush();
+            bw.close();
+
+        }
+        catch (ImpossivelAberturaDeArquivoException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }catch(IOException ex){
+            JOptionPane.showMessageDialog(null, "OCorreu um erro não esperado. Por favor, contate o suporte.");
+            ex.printStackTrace();
+        } 
+    }
+
+    public static BufferedReader lerArquivo(String nomeDoArquivo) throws ImpossivelAberturaDeArquivoException{
+        File file = new File(nomeDoArquivo);
+        try{
+            return new BufferedReader(new FileReader(nomeDoArquivo));
+        } catch (FileNotFoundException e){
+            throw new ImpossivelAberturaDeArquivoException(file.getName(), file.getPath());
+        }
+        
+    }
+}
+
+class ImpossivelAberturaDeArquivoException extends Exception {
+    private String nomeDoArquivo;
+    private String diretorio;
+
+    public ImpossivelAberturaDeArquivoException(String nomeDoArquivo, String diretorio){
+        super("O arquivo " + nomeDoArquivo + " não foi encontrado no diretório " + diretorio);
+        this.nomeDoArquivo = nomeDoArquivo;
+        this.diretorio = diretorio;
+    }
+/* 
+    @Override
+    public String toString(){
+        return "ImpossivelAberturaDeArquivoException{" + "nomeDoArquivo='" + nomeDoArquivo + '\'' + ", diretorio='" + diretorio + '\'' + '}';
+    }*/
+
 }
